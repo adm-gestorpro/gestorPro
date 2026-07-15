c40dc401621a3d466a977dce7d806ccd69ceabb1
Giba Junior <gibajr18@gmail.com>
2026-07-15 15:19:50 -0300

    Adição de mais opções de quantidade de lotes por página na listagem

--- produtos/views.py
+++ produtos/views.py
@@ -214,7 +214,7 @@ def controle_validade(request):
     filtro_loja = request.GET.get('loja', '').strip()
     query = request.GET.get('q', '').strip()
     per_page = request.GET.get('per_page', '50')
-    if per_page not in ['50', '100', '200']: 
+    if per_page not in ['50', '100', '200', '500', '1000']: 
         per_page = '50'
 
     hoje = date.today()
